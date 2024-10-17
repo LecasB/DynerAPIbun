cad73b25c85890ee24d322d494fec21650adb8bf
Leonardo <cleonardo191@gmail.com>
2024-10-17 01:17:51 +0100

    ola

--- firebase.js
+++ firebase.js
@@ -1,12 +1,19 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getDatabase } from "firebase-admin/database";
+import dotenv from "dotenv";
 
-import serviceAccount from "./serviceAccountKey.json";
+// Load environment variables from .env
+dotenv.config();
 
+// Initialize Firebase using environment variables
 initializeApp({
-  credential: cert(serviceAccount),
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
   databaseURL:
-    "https://dyner-a395a-default-rtdb.europe-west1.firebasedatabase.app/",
+    "https://dyner-a395a-default-rtdb.europe-west1.firebasedatabase.app",
 });
 
 const db = getDatabase();
